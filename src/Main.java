d4ec0e0542eb949dcbdbbcbda870d88c8ea105d2
martinbv99 <martinbv@mail.uc.edu>
2024-09-17 13:31:44 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        boolean done = true;
+        String name = "Naomi";
+        int age = 17;
+
+        System.out.println("Hello " + name);
+        System.out.println("Your age is " + age);
+        System.out.println(done);
+    }
+}
